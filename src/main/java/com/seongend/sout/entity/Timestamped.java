608742d2644c203074b2dbc53c5d154e8a0102c3
sixthh-sense <93525885+sixthh-sense@users.noreply.github.com>
2021-12-06 22:11:31 +0900

    3rd

--- src/main/java/com/seongend/sout/entity/Timestamped.java
+++ src/main/java/com/seongend/sout/entity/Timestamped.java
@@ -1,10 +1,5 @@
 package com.seongend.sout.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -19,14 +14,8 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class) //변경 시 자동 기록 ->
 public abstract class Timestamped {
     @CreatedDate
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createAt; //글작성 최초 시점
 
     @LastModifiedDate
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @JsonSerialize(using = LocalDateTimeSerializer.class)
-    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime modifiedAt; //글작성 최초 시점
 }
