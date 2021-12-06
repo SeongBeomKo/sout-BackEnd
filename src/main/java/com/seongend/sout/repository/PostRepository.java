ff502773f98d46e8896a03c88b78c0d50f800f9c
davidko <ksb3602@gmail.com>
2021-12-06 20:28:58 +0900

    Get all post requirement is done

--- src/main/java/com/seongend/sout/repository/PostRepository.java
+++ src/main/java/com/seongend/sout/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package com.seongend.sout.repository;
 
+import com.seongend.sout.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository<Post> extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
