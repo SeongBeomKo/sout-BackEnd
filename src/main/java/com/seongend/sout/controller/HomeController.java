321b2e93e14b8c888353b18e620bfa63478683a9
davidko <ksb3602@gmail.com>
2021-12-07 15:41:55 +0900

    Pagination is applied to search post api and get all post api

--- src/main/java/com/seongend/sout/controller/HomeController.java
+++ src/main/java/com/seongend/sout/controller/HomeController.java
@@ -3,6 +3,9 @@ package com.seongend.sout.controller;
 import com.seongend.sout.dto.PostResponseDto;
 import com.seongend.sout.service.HomeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,16 +17,16 @@ public class HomeController {
 
     private final HomeService homeService;
 
-
     @GetMapping("/")
-    public List<PostResponseDto> getAllPosts() {
+    public List<PostResponseDto> getAllPosts(@RequestParam int page, @RequestParam int size) {
         //@AuthenticationPrincipal UserDetailsImpl userDetails
-        return homeService.findAllPost();
+        Pageable sortedByModifiedAtDesc = PageRequest.of(page, size, Sort.by("modifiedAt").descending());
+        return homeService.findAllPosts(sortedByModifiedAtDesc);
     }
 
     @GetMapping("/search")
-    public List<PostResponseDto> getSearchedPost(@RequestParam String keyword) {
-        return homeService.searchPosts(keyword);
+    public List<PostResponseDto> getSearchedPost(@RequestParam String keyword, @RequestParam int page, @RequestParam int size) {
+        Pageable sortedByModifiedAtDesc = PageRequest.of(page, size, Sort.by("modifiedAt").descending());
+        return homeService.searchPosts(keyword, sortedByModifiedAtDesc);
     }
-
 }
--- src/main/java/com/seongend/sout/repository/PostRepository.java
+++ src/main/java/com/seongend/sout/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package com.seongend.sout.repository;
 
 import com.seongend.sout.entity.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findAllByOrderByModifiedAtDesc();
+    Page<Post> findAllByOrderByModifiedAtDesc(Pageable pageable);
 }
